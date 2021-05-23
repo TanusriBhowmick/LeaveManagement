906920f5a05ee9e7234c41914a7a083ef8bb41cf
Riorke <66472195+Riorke@users.noreply.github.com>
2021-05-23 06:43:42 -0700

    Add files via upload

---
+++ manager/includes/sidebar.php
@@ -0,0 +1,54 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Manager</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    
+                
+                 
+                            <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Employees<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Add Employee</a></li>
+                                <li><a href="manageemployee.php">Manage Employee</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">All Leaves </a></li>
+                                <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
+                                <li><a href="approvedleave-history.php">Approved Leaves</a></li>
+                                  <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+                            </li>  
+
+
+                        
+
+                 
+              
+                </ul> 
+                </div>
+            </aside>
